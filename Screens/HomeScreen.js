4cd8c1d4286022db20458c14ebcf77844b7dc1d2
chollid <chrisuga@me.com>
2021-05-17 21:37:59 -0400

    Added AdChat Screen, Signout functionality

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -4,10 +4,16 @@ import { SafeAreaView } from "react-native";
 import { TouchableOpacity } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
 import { Avatar } from "react-native-elements/dist/avatar/Avatar";
+import { AntDesign, SimpleLineIcons } from "@expo/vector-icons";
 import CustomListItem from "../components/CustomListItem";
 import { auth, db } from "../firebase";
 
 const HomeScreen = ({ navigation }) => {
+  const signOutUser = () => {
+    auth.signOut().then(() => {
+      navigation.replace("Login");
+    });
+  };
   useLayoutEffect(() => {
     navigation.setOptions({
       title: "Signal",
@@ -16,7 +22,7 @@ const HomeScreen = ({ navigation }) => {
       headerTintColor: "black",
       headerLeft: () => (
         <View style={{ marginLeft: 20 }}>
-          <TouchableOpacity activeOpacity={0.5}>
+          <TouchableOpacity activeOpacity={0.5} onPress={signOutUser}>
             <Avatar
               outlined
               rounded
@@ -29,8 +35,28 @@ const HomeScreen = ({ navigation }) => {
           </TouchableOpacity>
         </View>
       ),
+      headerRight: () => (
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: 80,
+            marginRight: 20,
+          }}
+        >
+          <TouchableOpacity activeOpacity={0.5}>
+            <AntDesign name="camerao" size={24} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("AddChat")}
+            activeOpacity={0.5}
+          >
+            <SimpleLineIcons name="pencil" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+      ),
     });
-  }, []);
+  }, [navigation]);
 
   return (
     <SafeAreaView>
